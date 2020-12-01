ec36917e41a8a61e116a3db12449634759ce3ac0
Lungisa Lucky Mkhasakhasa <216060117@mycput.ac.za>
2020-12-01 10:33:18 +0200

    update

--- src/main/java/com/adp3/controller/HomeController.java
+++ src/main/java/com/adp3/controller/HomeController.java
@@ -1,7 +1,16 @@
 package com.adp3.controller;
 
+import com.adp3.entity.standalone.Employee;
+import com.adp3.repository.standalone.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Author: Megan Jacobs
@@ -17,8 +26,34 @@ public class HomeController {
      * @param: baseUrl (localhost:8080/home)
      * @return: EmployeeTimeManagement Application as /home
      * */
+    @Autowired
+    EmployeeRepository repo;
+
     @RequestMapping({"/", "/index","/home"})
-    String home(){
-        return "EmployeeTimeManagement Application... ";
+    @ResponseBody
+    public String indexPage(Model model){
+        List<Employee> emp=(List<Employee>) repo.findAll();
+        model.addAttribute("empllist",emp);
+        return "index";
+
     }
+
+
+//    @RequestMapping({"/", "/index","/home"})
+//    @ResponseBody
+//    public String index(){
+//        return "you in";
+//    }
+//
+//    //login form
+//    @RequestMapping("/index.html")
+//    public String login(){
+//        return "index.html";
+//    }
+//    //login form
+//    @RequestMapping("/index.html")
+//    public String loginError(Model model){
+//        model.addAttribute("loginError", true);
+//        return "index.html";
+//    }
 }
